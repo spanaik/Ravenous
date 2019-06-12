f0f86a0b3a3734b8fc57a76f62429c315fbef905
spanaik <span_25862@yahoo.co.in>
2019-06-12 19:41:50 +0530

    Updates

---
+++ precache-manifest.5311019abbe696b43209e087a81bc99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ba72085cd0de5fae681e291deb71c0",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "b49fd887c30e5a63cfd8",
+    "url": "/Ravenous/static/css/main.98066aa7.chunk.css"
+  },
+  {
+    "revision": "bb925ff48b846a8400a2",
+    "url": "/Ravenous/static/js/2.4bcdc629.chunk.js"
+  },
+  {
+    "revision": "b49fd887c30e5a63cfd8",
+    "url": "/Ravenous/static/js/main.01e1fe81.chunk.js"
+  },
+  {
+    "revision": "5ddfa0b767d8254e6289",
+    "url": "/Ravenous/static/js/runtime~main.9078551a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
